aff6fdc3cb9ff6612864a4f06b0adb944dd2fea5
swetha242 <sswetha.242@gmail.com>
2018-11-10 22:23:01 +0530

    Added notes upload and download

--- backend/backend.py
+++ backend/backend.py
@@ -4,12 +4,15 @@ from flask_pymongo import PyMongo
 from bson.json_util import dumps
 from bson.objectid import ObjectId
 import os
+import random
+import base64
 from werkzeug.utils import secure_filename
+from datetime import datetime
 
 app = Flask(__name__)
 CORS(app)
 UPLOAD_FOLDER = 'files'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf'])
+ALLOWED_EXTENSIONS = set(['pdf'])
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config["MONGO_URI"] = "mongodb://localhost:27017/P2Pdb"
 mongo = PyMongo(app)
@@ -19,7 +22,10 @@ mongo = PyMongo(app)
 @app.route('/v1/signup',methods=['POST'])
 def adduser():
     data=request.get_json()
-    userdata = {'name':data['username'] ,'password':data['password'],'email': data['email'],'is_student':1}
+    user1=mongo.db.users.find_one({'email':data['email']})
+    if user1:
+        return jsonify({'result':"Already registered"})
+    userdata = {'name':data['username'] ,'password':data['password'],'email': data['email'],'is_student':1,'ques_ask':0,'ques_ans':0,'notes_upl':0,'view_notes':0}
     user=mongo.db.users.insert_one(userdata)
     if user:
         query={
@@ -52,7 +58,7 @@ def check_user():
         return jsonify({'result':"Invalid password"})
     else:
         return jsonify({'result':"Invalid user"})
-
+#-------------------------------NOTES------------------------------------------------------
 #notes with a particular tag
 @app.route('/notes/<tag>')
 def get_notes(tag):
@@ -89,21 +95,41 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/uploader', methods = ['GET', 'POST'])
+@app.route('/notes/upload', methods = ['GET', 'POST'])
 def upload_file():
-   if request.method == 'POST':
-        if 'file' not in request.files:
-            #flash('No file part')
-            return redirect(request.url)
-        f = request.files['file']
-        if f.filename == '':
-            #flash('No selected file')
-            return "no file selected"
-        if f and allowed_file(f.filename):
-            filename = secure_filename(f.filename)
-            print(filename)
-            f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return 'file uploaded successfully'   
+    data=request.get_json()
+    if data['islink']==0:
+            x=bytes(data['data'],encoding="UTF-8")
+            file_decode=base64.decodebytes(x)
+            file_name=str(random.randrange(100,10000000,1))+".pdf"
+            file1=open(os.path.join(app.config['UPLOAD_FOLDER'], file_name),'wb')
+            file1.write(file_decode)
+            file1.close()
+            data['data']=file_name
+    query={'subject':data['subject'],'course':1,'upvotes':0,'downvotes':0,'title':data['title'],'summary':data['summary'],'link':data['data'],'time':datetime.now()}
+    notes_ins=mongo.db.notes.insert_one(query)
+    if notes_ins:
+        v=mongo.db.users.find_one({"_id":ObjectId(data['userid'])})['notes_upl']
+        mongo.db.users.update({"_id":ObjectId(data['userid'])},{"$set":{'notes_upl':v+1}})
+        return jsonify({'result':'Success'})
+    else:
+        return jsonify({'result':'Error'})
+
+@app.route('/notes/view',methods=['GET','POST'])
+def view_file():
+    data=request.get_json()
+    print(data['notesid'])
+    notes_query=mongo.db.notes.find_one({'_id':ObjectId(data['notesid'])})
+    if notes_query:
+        file_name=notes_query['link']
+        file1=open(os.path.join(app.config['UPLOAD_FOLDER'], file_name),'rb')
+        file1=file1.read()
+        enc=base64.encodebytes(file1)
+        enc=enc.decode("utf-8").strip()
+        v=mongo.db.users.find_one({"_id":ObjectId(data['userid'])})['view_notes']
+        mongo.db.users.update({"_id":ObjectId(data['userid'])},{"$set":{'view_notes':v+1}})
+        return jsonify({'result':"Success",'data':enc})
+    return jsonify({'result':'Error'})
 
 '''------------------------------------IDEAS SECTION-------------------------------------------'''
 
