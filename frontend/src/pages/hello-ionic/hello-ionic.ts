17406b98eab0828da970ff359d14e07c284919cc
nishantsondhi <sondhi.nishant@gmail.com>
2018-11-10 22:25:12 +0530

    Removing login and signup from toolbar.

--- frontend/src/pages/hello-ionic/hello-ionic.ts
+++ frontend/src/pages/hello-ionic/hello-ionic.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { NavController, AlertController, LoadingController, Loading, IonicPage } from 'ionic-angular';
+import { SignupPage } from '../signup/signup';
+import { LoginPage } from '../login/login';
+
 
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
-  constructor() {
 
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  Login(){
+    this.navCtrl.push(LoginPage);
+  }
+  Signup(){
+    this.navCtrl.push(SignupPage);
   }
 }
