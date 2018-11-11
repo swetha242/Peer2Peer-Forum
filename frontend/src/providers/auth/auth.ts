33075fa4136164d6f91f1c7710a045c98aa09592
ThePilot916 <shreyasmiraj8@gmail.com>
2018-11-11 17:22:14 +0530

    dynamic form added

--- frontend/src/providers/auth/auth.ts
+++ frontend/src/providers/auth/auth.ts
@@ -3,49 +3,50 @@ import { Http, Headers } from '@angular/http';
 import { Storage } from '@ionic/storage';
 import * as Enums from '../../assets/apiconfig';
 import 'rxjs/add/operator/map';
- 
+
 @Injectable()
 export class AuthProvider {
- 
+
   public token: any;
- 
+
   constructor(public http: Http, public storage: Storage) {
- 
+
   }
- 
+
   checkAuthentication(){
- 
+
     return new Promise((resolve, reject) => {
- 
+
         //Load token if exists
         this.storage.get('token').then((value) => {
- 
+
             this.token = value;
- 
+
             let headers = new Headers();
             headers.append('Authorization', this.token);
- 
+
             let url = Enums.APIURL.URL1;
             let path = url.concat( "/v1/auth");
             //console.log(path);
             this.http.get(path)
                 .subscribe(res => {
                     resolve(res);
+                    console.log(resolve(res));
                 }, (err) => {
                   console.log("yo");
                     reject(err);
                 });
- 
-        });        
- 
+
+        });
+
     });
- 
+
   }
- 
+
   createAccount(details){
- 
+
     return new Promise((resolve, reject) => {
- 
+
         let headers = new Headers();
         headers.append('Content-Type', 'application/json');
 
@@ -56,28 +57,28 @@ export class AuthProvider {
 
         this.http.post(path, details, {headers: headers})
           .subscribe(res => {
- 
+
             let data = res.json();
             console.log(data)
             //this.token = data.token;
             //this.storage.set('token', data.token);
             resolve(data);
- 
+
           }, (err) => {
             console.log(err);
             reject(err);
           });
- 
+
     });
- 
+
   }
- 
+
   login(credentials){
- 
+
     return new Promise((resolve, reject) => {
 
       //console.log(credentials);
- 
+
         let headers = new Headers();
         headers.append('Content-Type', 'application/json');
 
@@ -85,7 +86,7 @@ export class AuthProvider {
         let url = Enums.APIURL.URL1;
         let path = url.concat("/v1/auth");
         //console.log(path);
- 
+
         this.http.post(path, JSON.stringify(credentials), {headers: headers})
           .subscribe(res => {
             //console.log(res['_body']);
@@ -94,18 +95,18 @@ export class AuthProvider {
             //this.token = data.result;
             //this.storage.set('token', data.result);
             resolve(data);
- 
+
           }, (err) => {
             console.log("yo");
             reject(err);
           });
- 
+
     });
- 
+
   }
- 
+
   logout(){
     this.storage.set('token', '');
   }
- 
+
 }
