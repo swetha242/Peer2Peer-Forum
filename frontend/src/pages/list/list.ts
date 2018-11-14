2f36673f4de496e2c5046cbff048c5f98f1a2786
nishantsondhi <sondhi.nishant@gmail.com>
2018-11-14 11:54:36 +0530

    Added answer listing,upvote and downvote.Connected to backend as well.

--- frontend/src/pages/list/list.ts
+++ frontend/src/pages/list/list.ts
@@ -12,6 +12,8 @@ import { Storage } from '@ionic/storage';
 export class ListPage {
   items: Array<{description:string,subject:string,qid:string,owner:string,upvotes:number,downvotes:number,title:string,tag:Array<"">}>;
   answers: Array<{answeredby : string,teacher: number,content:string,upvote:number,downvote:number,answeredbyname:string}>;
+  items1: Array<{description:string,subject:string,qid:string,owner:string,upvotes:number,downvotes:number,title:string,tag:Array<"">}>;
+  //items2 : Array<""> 
   //the question asked by user
   question:string;
   title:string;
@@ -22,6 +24,7 @@ export class ListPage {
   constructor(public navCtrl: NavController, public navParams: NavParams,public http: Http, public storage: Storage) {
 
     this.items = [];
+    this.items1 = []
     this.answers=[];
     console.log(this.userid);
     //console.log(this.subject);
@@ -50,8 +53,8 @@ export class ListPage {
                 tag : data[i].tags,
                 title : data[i].title,
                 description : data[i].description
-              }
-              );
+              });
+              
             
             }
             // let ques=data['questions'];
@@ -112,12 +115,12 @@ export class ListPage {
                 answeredby : data[i].answered_by,
                 teacher : data[i].teacher,
                 content : data[i].content,
-                upvote : data[i].upvote,
-                downvote: data[i].downvote,
+                upvote : data[i].upvotes,
+                downvote: data[i].downvotes,
                 answeredbyname: data[i].answered_by_n
                 }
               )}
-            
+            console.log(this.answers)
             //this.token = data.token;
             //this.storage.set('token', data.token);
             //resolve(data);
@@ -129,6 +132,97 @@ export class ListPage {
             //reject(err);
           });
   
-  }
+    }
+    initializeItems(ev){
+     
+      let postParams = {description : this.question,asked_by:this.userid,subject:this.subject,title:this.title,tags:this.tags}
+      let headers = new Headers();
+      headers.append('Content-Type', 'application/json');
+ 
+          let url = Enums.APIURL.URL1;
+          let path = url.concat( "/qa/qlist");
+          console.log(postParams);
+ 
+          this.http.post(path, postParams, {headers: headers})
+            .subscribe(res => {
+             console.log(res)
+              let data = res.json()['question'];
+             for(let i in data){
+                 this.items1.push({
+                 subject : data[i].subject,
+                 qid : data[i]._id,
+                 owner : data[i].asked_by_n,
+                 upvotes : data[i].upvotes,
+                 downvotes: data[i].downvotes,
+                 tag : data[i].tags,
+                 title : data[i].title,
+                 description : data[i].description
+               });
+               console.log('initialize')
+               console.log(this.items1)
 
+               
+             
+             }
+             // let ques=data['questions'];
+              //traverse the questions array
+
+         
+              console.log('out')
+              console.log(this.items1)
+              console.log('val')
+              console.log(ev.target.value)
+              var val = ev.target.value
+              if (val && val.trim() != '') {
+                this.items1 = this.items1.filter((item) => {
+                  console.log('here')
+                  console.log(item.description.toLowerCase().indexOf(val.toLowerCase()) > -1)
+                  console.log('finished')
+                  return (item.description.toLowerCase().indexOf(val.toLowerCase()) > -1);
+                })
+              }
+ 
+            }, (err) => {
+              console.log(err);
+ 
+            });
+
+            console.log('out1')
+            console.log(this.items1)
+   /*   this.items = [
+        'Chemistry',
+        'Physics',
+        'Maths',
+        'Operating System',
+        'Computer Network',
+        'Software Engineering'
+      ];*/
+    }
+    getItems(ev) {
+      // Reset items back to all of the items
+      this.initializeItems(ev);
+  //  console.log(ev.target.description)
+      // set val to the value of the ev target
+/*      var val = ev.target.value;
+      console.log('this is val')
+      console.log(val)
+      console.log('here')
+      console.log(this.items1)
+      console.log('finished')
+      if (val && val.trim() != '') {
+        this.items1 = this.items1.filter((item) => {
+          console.log('here')
+          console.log(item.description.toLowerCase().indexOf(val.toLowerCase()) > -1)
+          console.log('finished')
+          return (item.description.toLowerCase().indexOf(val.toLowerCase()) > -1);
+        })
+      }
+    
+ /*   choosesub(it)
+    {
+      this.navCtrl.push(ListPage,{ description: this.q});
+    }*/
+
+
+  }
 }
