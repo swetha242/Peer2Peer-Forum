cca38fc658963bf7425878486f5e8fbd42a8da6d
swetha242 <sswetha.242@gmail.com>
2018-11-14 12:34:35 +0530

    inserted sample jsons

--- frontend/src/pages/notes/notes.ts
+++ frontend/src/pages/notes/notes.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Http, Headers } from '@angular/http';
 import { ViewnotesPage} from '../viewnotes/viewnotes';
-import * as Enums from '../../assets/apiconfig';
 /**
  * Generated class for the NotesPage page.
  *
@@ -16,48 +15,20 @@ import * as Enums from '../../assets/apiconfig';
   templateUrl: 'notes.html',
 })
 export class NotesPage {
-  subject=this.navParams.get('subject');
-  userid=this.navParams.get('userid');
-  
 
-<<<<<<< HEAD
   
-=======
-  items: Array<{title: string, author: string, number : number, qtext : string}>;
->>>>>>> 56bed20f05c62171d1caffaf3ce56c850f9c4371
   constructor(public navCtrl: NavController, public navParams: NavParams,public http: Http) {
-    this.items = [];
-    let url = Enums.APIURL.URL1;
-    let request = "/notes/".concat(this.subject);
-    let path = url.concat(request);
-    console.log(path);
-
-    for(let i = 1; i < 11; i++) {
-      this.items.push({
-        number : i,
-        title: 'Notes Title',
-        author: 'abc',
-        qtext : "A small description of the notes",
-
-      });
-    }
   }
-
   base64: any;
   userid=this.navParams.get('userid')
   subject=this.navParams.get('subject')
   fun(b64Data,ty)
   {
     //console.log(b64Data)
-    //console.log(this.userid);
     let x=b64Data.split(',')[1]
     //http call to upload notes where x is base64 rep- when islink is 1 data will have link- anyways not sure if link will work
     console.log(this.userid)
-<<<<<<< HEAD
     let postParams = {'data':x,'userid':this.userid,'subject':this.subject,'tag':'Neural Networks','course':1,'title':'ANN','summary':'xxxx','islink':0}
-=======
-    let postParams = {'data':x,'userid':this.userid,'subject':this.subject,'title':'Reverse Ajax','summary':'xxxx','islink':0}
->>>>>>> 56bed20f05c62171d1caffaf3ce56c850f9c4371
      let headers = new Headers();
      headers.append('Content-Type','application/json');
  
@@ -86,6 +57,8 @@ export class NotesPage {
               //console.log(self.base64);
               self.fun(self.base64,file.type);
             }, false);
+
+            
             // Convert data to base64
             fileReader.readAsDataURL(file);
   //console.log(postParams)
@@ -124,8 +97,4 @@ export class NotesPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad NotesPage');
   }
-    itemTapped(event, item) {
-    this.navCtrl.push(ViewnotesPage, { item: item }
-    );
   }
-}
